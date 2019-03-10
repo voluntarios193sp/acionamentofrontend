fad5b40e41a26f749c5bd78a48451d2fe36cd889
Jeff Prestes <jeffprestes@gmail.com>
2019-03-10 15:36:20 -0300

    submete ocorrencia

--- js/bombeiro.js
+++ js/bombeiro.js
@@ -19,23 +19,29 @@ function obterLocalizacao(location) {
     dado.data_ocorrencia = dateObj.getUTCFullYear() + "-" + (dateObj.getUTCMonth() + 1) + "-" + dateObj.getUTCDate();
     dado.longitude = location.coords.longitude;
     dado.latitude = location.coords.latitude;
-    dado.telefone_solicitante = document.frmOcorrencia.telefone;
+    dado.telefone_solicitante = document.frmOcorrencia.telefone.value;
     dado.nome_solicitante = "COBOM averiguar";
     dado.cpf_solicitante = "COBOM averiguar";
     dado.natureza = "incendio,casa";
-    dado.descricao = document.frmOcorrencia.detalhes;
+    dado.descricao = document.frmOcorrencia.detalhes.value;
 
     $.ajax({
         type: "POST",
-        url: "",
+        url: "https://www.novatrix.com.br/cobomocorrencias/ocorrencia",
         // The key needs to match your method's input parameter (case-sensitive).
-        data: JSON.stringify({ Markers: markers }),
+        data: JSON.stringify(dado),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(data){alert(data);},
+        success: sucesso,
         failure: function(errMsg) {
-            alert(errMsg);
+            console.log("Erro no envio: ", errMsg);
+            alert("Houve um problema na comunicação. Por favor ligue 193");
         }
     });
-    
 }
+
+function sucesso(data) {
+    console.log("retorno: ", data);
+    document.location = "registrado.html";
+}
+
